728374a8be27c4351715b4b6707384127c9bc607
Nicolás Fantone <nfantone@gmail.com>
2016-04-10 01:14:37 -0300

    Add eslint rules to match those of koajs/koa

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 'use strict';
-var defaults = require('lodash.defaults');
-var assign = require('lodash.assign');
+const defaults = require('lodash.defaults');
+const assign = require('lodash.assign');
 
-var props = [
+const props = [
   'name',
   'message',
   'stack',
-  'type',
-]
+  'type'
+];
 
-var DEFAULTS = {
+const DEFAULTS = {
   format: function(err) {
     // set all enumerable properties of error onto the object
-    var obj = assign({}, err)
+    let obj = assign({}, err);
+    props.forEach(key => {
+      let value = err[key];
+      if (value) obj[key] = value;
+    });
 
-    props.forEach(function(key) {
-      var value = err[key]
-      if (value) obj[key] = value
-    })
+    obj.status = err.status || err.statusCode || 500;
 
-    obj.status = err.status || err.statusCode || 500
-
-    return obj
+    return obj;
   }
-}
+};
 
 module.exports = function(options) {
   options = defaults({}, options, DEFAULTS);
 
-  return function* jsonErrorHandler(next) {
-    var status
+  return function * jsonErrorHandler(next) {
+    let status;
     try {
-      yield * next
+      yield * next;
 
-      status = this.response.status
+      status = this.response.status;
       // future proof status
       if (!status || (status === 404 && this.response.body == null)) {
-        this.throw(404)
+        this.throw(404);
       }
     } catch (err) {
       // set body
@@ -45,12 +44,12 @@ module.exports = function(options) {
       // set status
       status =
         this.response.status =
-        err.status || err.statusCode || 500
+        err.status || err.statusCode || 500;
 
       // emit the error if we really care
       if (!err.expose && status >= 500) {
-        this.app.emit('error', err, this)
+        this.app.emit('error', err, this);
       }
     }
-  }
-}
+  };
+};
--- test/test.js
+++ test/test.js
@@ -1,148 +1,151 @@
 'use strict';
-var request = require('supertest')
-var assert = require('assert')
-var koa = require('koa')
+const request = require('supertest');
+const assert = require('assert');
+const koa = require('koa');
 
-var error = require('..')
+const error = require('..');
 
-describe('with default options', function () {
-  it('should show the stack', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      throw new Error()
-    })
+describe('with default options', () => {
+  it('should show the stack', done => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      throw new Error();
+    });
     request(app.listen())
     .get('/')
-    .expect(500, function (err, res) {
-      if (err) return done(err)
+    .expect(500, (err, res) => {
+      if (err) return done(err);
 
-      assert(res.body)
-      done()
-    })
-  })
+      assert(res.body);
+      done();
+    });
+  });
 
-  it('should show the name', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      throw new Error()
-    })
+  it('should show the name', done => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      throw new Error();
+    });
     request(app.listen())
     .get('/')
-    .expect(500, function (err, res) {
-      if (err) return done(err)
+    .expect(500, (err, res) => {
+      if (err) return done(err);
 
-      assert.equal('Error', res.body.name)
-      done()
-    })
-  })
+      assert.equal('Error', res.body.name);
+      done();
+    });
+  });
 
-  it('should show the message', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      throw new Error('boom')
-    })
+  it('should show the message', done => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      throw new Error('boom');
+    });
     request(app.listen())
     .get('/')
-    .expect(500, function (err, res) {
-      if (err) return done(err)
+    .expect(500, (err, res) => {
+      if (err) {
+        return done(err);
+      }
+      assert.equal('boom', res.body.message);
+      done();
+    });
+  });
 
-      assert.equal('boom', res.body.message)
-      done()
-    })
-  })
-
-  it('should show the status', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      this.throw(404)
-    })
+  it('should show the status', (done) => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      this.throw(404);
+    });
     request(app.listen())
     .get('/')
-    .expect(404, function (err, res) {
-      if (err) return done(err)
+    .expect(404, (err, res) => {
+      if (err) {
+        return done(err);
+      }
+      assert.equal('Not Found', res.body.message);
+      assert.equal(404, res.body.status);
+      done();
+    });
+  });
 
-      assert.equal('Not Found', res.body.message)
-      assert.equal(404, res.body.status)
-      done()
-    })
-  })
-
-  it('should check for err.statusCode', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      var err = new Error('boom')
-      err.statusCode = 501
-      throw err
-    })
-
-    request(app.listen())
-    .get('/')
-    .expect(501, done)
-  })
-
-  it('should emit errors', function (done) {
-    var app = koa()
-    app.use(error())
-    app.use(function* () {
-      throw new Error('boom')
-    })
-
-    app.once('error', function (err, ctx) {
-      assert.equal('boom', err.message)
-      assert.equal(500, ctx.status)
-      done()
-    })
+  it('should check for err.statusCode', done => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      let err = new Error('boom');
+      err.statusCode = 501;
+      throw err;
+    });
 
     request(app.listen())
     .get('/')
-    .expect(500, function () {})
-  })
-})
+    .expect(501, done);
+  });
 
-describe('with custom format function', function() {
-  it('should allow defining a format function', function () {
-    var options = {
+  it('should emit errors', done => {
+    let app = koa();
+    app.use(error());
+    app.use(function * () {
+      throw new Error('boom');
+    });
+
+    app.once('error', (err, ctx) => {
+      assert.equal('boom', err.message);
+      assert.equal(500, ctx.status);
+      done();
+    });
+
+    request(app.listen())
+    .get('/')
+    .expect(500, () => {});
+  });
+});
+
+describe('with custom format function', () => {
+  it('should allow defining a format function', () => {
+    let options = {
       format: Function.prototype
-    }
+    };
 
-    var app = koa()
-    assert.doesNotThrow(function () {
-      app.use(error(options))
-    })
-  })
+    let app = koa();
+    assert.doesNotThrow(() => {
+      app.use(error(options));
+    });
+  });
 
-  it('should respect custom format while preserving status', function(done) {
-    var options = {
-      format: function(err) {
+  it('should respect custom format while preserving status', done => {
+    let options = {
+      format: () => {
         return {
           status: 200,
           message: 'OK'
-        }
+        };
       }
-    }
+    };
 
-    var app = koa()
-    app.use(error(options))
-    app.use(function* () {
-      var err = new Error('boom')
-      err.statusCode = 501
-      err.customField = 'fatal'
-      throw err
-    })
+    let app = koa();
+    app.use(error(options));
+    app.use(function * () {
+      let err = new Error('boom');
+      err.statusCode = 501;
+      err.customField = 'fatal';
+      throw err;
+    });
 
     request(app.listen())
     .get('/')
-    .expect(501, function (err, res) {
-      if (err) return done(err)
-
-      assert.equal('OK', res.body.message)
-      assert.equal(200, res.body.status)
-      done()
-    })
-  })
-})
+    .expect(501, (err, res) => {
+      if (err) {
+        return done(err);
+      }
+      assert.equal('OK', res.body.message);
+      assert.equal(200, res.body.status);
+      done();
+    });
+  });
+});
